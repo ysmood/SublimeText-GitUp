7e83c9cf36391cce3eb94914e85c3b252697bedf
Yad Smood <y.s.inside@gmail.com>
2015-09-23 02:29:54 +0800

    upd

--- gitup.py
+++ gitup.py
@@ -2,10 +2,10 @@ import sublime, sublime_plugin
 import subprocess
 
 settings = sublime.load_settings('gitup.sublime-settings')
-
+path = settings.get('path', '/usr/local/bin/gitup')
 class OpenGitupCommand(sublime_plugin.WindowCommand):
     def run(self):
+        global path
         folder = self.window.extract_variables()['folder']
-        path = settings.get('path', 'gitup')
         subprocess.Popen([path], cwd = folder)
         print("gitup opened")
