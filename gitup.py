01be3340f5c7987c0d69458abdc2607c20dd8b4d
Yad Smood <y.s.inside@gmail.com>
2015-09-25 07:08:33 +0800

    fix: a path issue

--- gitup.py
+++ gitup.py
@@ -1,16 +1,18 @@
 import sublime, sublime_plugin
 import subprocess
 
-settings = sublime.load_settings('gitup.sublime-settings')
-gitupPath = settings.get('path', '/usr/local/bin/gitup')
 
 class OpenGitupCommand(sublime_plugin.WindowCommand):
+    def __init__(self, arg):
+        settings = sublime.load_settings('gitup.sublime-settings')
+        self.gitupPath = settings.get('path', '/usr/local/bin/gitup')
+        sublime_plugin.WindowCommand.__init__(self, arg)
+
     def run(self):
-        global gitupPath
         try:
             path = self.window.extract_variables()['file_path']
         except:
             path = self.window.extract_variables()['folder']
 
-        subprocess.Popen([gitupPath], cwd = path)
+        subprocess.Popen([self.gitupPath], cwd = path)
         print("gitup opened")
